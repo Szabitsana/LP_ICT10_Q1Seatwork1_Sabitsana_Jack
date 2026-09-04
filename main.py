b158d0e85d9d5269be22e8f2458a5d796698d5ee
Jackie Chan <sabitsana.jap@obmontessori.edu.ph>
2026-09-04 11:39:31 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# --- BASIC DATA TYPES ---
+
+nameOne = "Arturo Santos" # string
+
+ageOne = 15 # integer
+
+heightOne = "167cm" # string
+
+
+student_type_one = False # boolean
+
+# --- CONTAINER TYPES ---
+
+# List
+ourCountries = ["Venezuela", "Israel", "Switzerland"]
+
+# Dictionary
+ourLists = {
+    "car_brand": "Tuk Tuk",
+    "shoe_size": 11,
+    "best_friend": "Jack Sabitsana"
+}
+
+print("joke")
+ourLists["best_friend"] = "Jack Sabitsana"
+
+# List
+ourFruits = ["Strawberry", "Tomato", "Mango", "Lychee"]
+
+# Tuple
+ourWeek = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
